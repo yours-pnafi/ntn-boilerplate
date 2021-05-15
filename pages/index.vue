e272ed045c84c8b5d03a7c349fdac323545d72d0
yours-pnafi <84247768+yours-pnafi@users.noreply.github.com>
2021-05-16 01:13:15 +0530

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -11,3 +11,14 @@
     </section>
   </main>
 </template>
+<script>
+  if (window.netlifyIdentity) {
+    window.netlifyIdentity.on("init", user => {
+      if (!user) {
+        window.netlifyIdentity.on("login", () => {
+          document.location.href = "/admin/";
+        });
+      }
+    });
+  }
+</script>
